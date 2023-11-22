9f6aa80f6ebed45d9e5199f217e1f5a8dfccbdec
Alberto Pietrangelo <abo.p@hotmail.com>
2023-11-22 12:58:47 +0100

    fix: rollup build options imported names

--- rollup.config.js
+++ rollup.config.js
@@ -1,59 +1,60 @@
+  
+
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-
-// const typescript = require('rollup-plugin-typescript2');
-const postcss = require('rollup-plugin-postcss'); 
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
+
+// To handle css files
+import postcss from "rollup-plugin-postcss";
+
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-const rdel = require('rollup-plugin-delete'); // delete dist every time
+
 
 const packageJson = require("./package.json");
 
-
 export default [
-    {
-      input: "src/index.ts",
-      output: [
-        /*{
-          file: packageJson.main,
-          format: 'cjs',
-          sourcemap: true,
-          name: '@famalabs/map-ui'
+  {
+    input: "src/index.ts",
+    output: [
+      /*{
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
       },*/
       {
-          //file: packageJson.module,
-          dir: 'dist',
-          format: 'esm',
-          sourcemap: true,
-          preserveModules: true,
-          preserveModulesRoot: 'src'
+            exports: 'named',
+            dir: 'dist',
+            format: 'esm',
+            sourcemap: true,
+            preserveModules: true,
+            preserveModulesRoot: 'src'
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs({requireReturnsDefault: 'esmExternals', defaultIsModuleExports: 'auto', esmExternals: true}), // option avoid 'default' export error
+      typescript({ tsconfig: "./tsconfig.json"}), // get declarations options from tsconfig
+      postcss(), 
+      terser(),
+    ],
+    onwarn(warning, warn) { // avoid mui warning
+      if (
+        warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+        warning.message.includes(`'use client'`)
+      ) {
+        return;
       }
-      ],
-      plugins: [
-        resolve(),
-        commonjs(),
-        peerDepsExternal(),
-        typescript({ tsconfig: "./tsconfig.json" }),
-        postcss(), 
-        terser()
-      ],
-      onwarn(warning, warn) {
-        if (
-          warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
-          warning.message.includes(`'use client'`)
-        ) {
-          return;
-        }
-        warn(warning);
-      }
-    },
-    {
-      input: "dist/types/index.d.ts",
-      output: [{ file: "dist/index.d.ts", format: "esm" }],
-      plugins: [dts()],
-      // external: [/\.css$/],
-    },
-    
-  ];
+      warn(warning);
+    }
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports 
+  },
+]
